1daf8bcb05b268eb48b621a5ab5dd0eaf8d3ee21
ines <ines@inesitapc>
2020-03-03 15:32:05 +0100

    nuestro proyecto de pogramacion

--- juegos/src/juegos/Juegos.java
+++ juegos/src/juegos/Juegos.java
@@ -18,6 +18,7 @@ public class Juegos {
         System.out.println("Zampa huevos");
         System.out.println("Bueno, pues ahora a sacar el trabajo de internet que ya hemos hecho bastante");
         System.out.println("Miau");
+       
     }
     
 }
