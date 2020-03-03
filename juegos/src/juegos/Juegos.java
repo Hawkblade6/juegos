e4857477199aee610f70d9e2c76de2b454d733e4
diego_l42v1g9 <diego_l42v1g9@LAPTOP-EN9S4AAP>
2020-03-03 09:03:49 +0100

    Commit de prueba 2

--- juegos/src/juegos/Juegos.java
+++ juegos/src/juegos/Juegos.java
@@ -12,6 +12,7 @@ public class Juegos {
     public static void main(String[] args) {
         
         System.out.println("PrimerCommit");
+        System.out.println("segundo Commit de Diego");
         
     }
     
